0f032713ceff8867d8441c30acf5a7db87bd85b8
Scherbakov Ilya <hitrojopic@gmail.com>
2023-04-06 00:56:44 +0300

    Initial commit
    Создание проекта на react, webpack, scss, babel, postcss

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports={
+    //Режим проекта и точка входа
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname,'./src/index.js'),
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+    stats: {
+        children: true,
+    },
+    //Настройки сервера
+    devServer: {
+        watchFiles: [path.resolve(__dirname,"src")],
+        historyApiFallback: true,
+        port:3000,
+        hot:true,
+    },
+    //Выходной main файл
+    output: {
+        filename: `[name].js`,
+        path: path.resolve(__dirname,'docs')
+    },
+    resolve: {
+        extensions: ['.ts', '.jsx', '.js', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader",'css-loader']
+            },
+            {
+                //Компиляция из sass в css
+                test: /\.sсss$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader', {
+                    loader: 'sass-loader',
+                    options: {
+                        additionalData: '@import "src/style.sсss";',
+                    }
+                }]
+            },
+            {
+                //Обработка картинок, иконок, svg
+                test: /\.(jpg|png|svg|ico|webp)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[path]/[name].[ext]'
+                    }
+                }]
+            },
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: path.resolve(__dirname,'./public/index.html'),
+        favicon: path.resolve(__dirname,'./public/favicon.ico')
+    }),
+        new CleanWebpackPlugin()
+    ]
+}
