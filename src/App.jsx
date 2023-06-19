d3f3e85e02d072df2867e4f7b039960085140221
Scherbakov Ilya <scherbakov720@gmail.com>
2023-06-19 17:26:24 +0300

    Refactor router props
    Изменен способ передачи роутера. Теперь он создается в index.jsx и передается в App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,16 @@ import {ErrorMessage} from "./components";
 import {Provider} from "react-redux";
 import {Route, Routes} from "react-router-dom";
 import {store, history} from "./redux/store";
-import {AddBookPage, BookList, BookPage, EditBookPage, ErrorPage} from "./pages";
 import {HistoryRouter} from "redux-first-history/rr6";
 
-function App() {
+function App({router}) {
     return (
         <Provider store={store}>
             <HistoryRouter history={history}>
                 <ErrorMessage/>
 
                 <Routes>
-                    <Route path="/" element={<BookList/>} />
-                    <Route path="/book/:bookId" element={<BookPage/>} />
-                    <Route path="/book/:bookId/edit" element={<EditBookPage/>} />
-                    <Route path="/book/add" element={<AddBookPage/>} />
-                    <Route element={<ErrorPage/>} />
+                    {router.map((page) => <Route key={page.path} path={page.path} element={page.element}/>)}
                 </Routes>
             </HistoryRouter>
         </Provider>
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
+import {AddBookPage, BookList, BookPage, EditBookPage, ErrorPage} from "./pages";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App/>);
+const router = [
+    {
+        path: '/',
+        element: <BookList/>
+    },
+    {
+        path: '/book/:bookId',
+        element: <BookPage/>,
+    },
+    {
+        path: '/book/:bookId/edit',
+        element: <EditBookPage/>,
+    },
+    {
+        path: '/book/add',
+        element: <AddBookPage/>,
+    },
+    {
+        path: '*',
+        element: <ErrorPage/>
+    }
+];
+
+root.render(<App router={router}/>);
